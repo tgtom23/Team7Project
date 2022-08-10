bf7e10daf3ef6c57fc9282eb4379eb93fba59125
user <user@this.com>
2022-08-10 13:58:00 -0400

    Initial Team Repo

---
+++ Team7MCCDataService/src/main/java/com/bah/msd/mcc/Team7MccDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team7MccDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team7MccDataServiceApplication.class, args);
+	}
+
+}
